7671f8ee383cc41d66bbe40aa65e2469b66db5a6
Hayato Kawai <fohte.hk@gmail.com>
2018-07-16 23:15:50 +0900

    chore(schema): set default values

--- db/schemas/maps.rb
+++ db/schemas/maps.rb
@@ -3,8 +3,8 @@
 create_table :maps, force: :cascade, id: :bigint, unsigned: true, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
   t.references :music, null: false, unsigned: true
 
-  t.integer :num_notes, null: false
-  t.integer :level, null: false
+  t.integer :num_notes, default: 0, null: false
+  t.integer :level, default: 0, null: false
   t.string :play_style, null: false # enum
   t.string :difficulty, null: false # enum
   t.integer :min_bpm, default: 0, null: false
