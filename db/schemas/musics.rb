6e0b6041e081c161a1191b94a6ce0451d0025db7
Hayato Kawai <fohte.hk@gmail.com>
2018-07-16 21:34:27 +0900

    feat(schema): add sub_name column to the musics table

--- db/schemas/musics.rb
+++ db/schemas/musics.rb
@@ -2,6 +2,7 @@
 
 create_table :musics, force: :cascade, id: :bigint, unsigned: true, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
   t.string :name, null: false
+  t.string :sub_name, null: false, default: ''
   t.string :genre, null: false
   t.string :artist, null: false
   t.string :textage_uid, null: false
