4b91c30924736eee8163f449d255fea686e1c2ed
Hayato Kawai <fohte.hk@gmail.com>
2018-07-16 21:34:05 +0900

    chore(config): add acronyms for AC and JavaScript

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym 'AC'
   inflect.acronym 'IIDX'
   inflect.acronym 'IO'
+  inflect.acronym 'JavaScript'
 end
