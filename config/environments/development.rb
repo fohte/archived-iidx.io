92e9ad8cb20d9cb8216ed0b7eea158b1d520a698
Hayato Kawai <fohte.hk@gmail.com>
2018-07-23 03:07:09 +0900

    chore(rails): output logs to log/development.log instead of STDOUT when RAILS_ENV is development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,9 +50,6 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.log_formatter = ::Logger::Formatter.new
-  logger = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
