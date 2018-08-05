29d950d6c8587131155d79ee3119c7e1454719e6
Hayato Kawai <fohte.hk@gmail.com>
2018-08-06 05:39:34 +0900

    build(docker-compose, frontend): use dotenv
    This commit supports using process.env

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,2 +1,9 @@
+const Dotenv = require('dotenv-webpack')
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+
+module.exports = withTypescript({
+  webpack(config, options) {
+    config.plugins.push(new Dotenv())
+    return config
+  },
+})
