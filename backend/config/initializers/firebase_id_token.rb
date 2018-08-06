2416320c82f1e8dddea9e5a4a9faba27dc4e9dbe
Hayato Kawai <fohte.hk@gmail.com>
2018-08-07 07:36:50 +0900

    build(backend, gem, docker-compose): add firebase_id_token instead of my jwt implementaiton

---
+++ backend/config/initializers/firebase_id_token.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+FirebaseIdToken.configure do |config|
+  config.project_ids = [ENV['FIREBASE_PROJECT_ID']]
+  config.redis = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'], db: 15)
+end
