89654412a706efc2b127622213ae879a5ec0194b
Hayato Kawai <fohte.hk@gmail.com>
2018-08-12 19:00:29 +0900

    feat(backend): allow CORS on /graphql

---
+++ backend/config/initializers/cors.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '/graphql',
+             headers: :any,
+             methods: %i[post options]
+  end
+end
