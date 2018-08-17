33372a2ad1f81d1713d847a9842dc6c31add2d4c
Hayato Kawai <fohte.hk@gmail.com>
2018-08-18 01:21:28 +0900

    feat(frontend): integrate antd and next.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,16 @@
 const Dotenv = require('dotenv-webpack')
+const withCSS = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
 
-module.exports = withTypescript({
-  webpack(config, options) {
-    config.plugins.push(new Dotenv({ path: '../.env', systemvars: true }))
-    return config
-  },
-})
+if (typeof require !== 'undefined') {
+  require.extensions['.css'] = file => {}
+}
+
+module.exports = withCSS(
+  withTypescript({
+    webpack(config, options) {
+      config.plugins.push(new Dotenv({ path: '../.env', systemvars: true }))
+      return config
+    },
+  }),
+)
