b86ede700461ff13ee5f04b7300a09213a922cc4
Hayato Kawai <fohte.hk@gmail.com>
2018-08-18 01:22:04 +0900

    feat(frontend): integrate styled-components and next.js

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,9 +1,14 @@
 import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  public static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+  public static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />),
+    )
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
   }
 
   public render() {
@@ -15,6 +20,7 @@ export default class MyDocument extends Document {
             content="width=device-width, initial-scale=1.0"
           />
           <link rel="stylesheet" href="/_next/static/style.css" />
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
