8f55401183f6fa65f9b48c65af626c720c3ee095
Hayato Kawai <fohte.hk@gmail.com>
2018-08-18 14:11:00 +0900

    fix(frontend): ignore no-empty rule

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const withCSS = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
 
 if (typeof require !== 'undefined') {
-  require.extensions['.css'] = file => {}
+  require.extensions['.css'] = file => {} // tslint:disable-line:no-empty
 }
 
 module.exports = withCSS(
