2fae5d9f6347f0e42992c459c48e3eb5c130853e
Hayato Kawai <fohte.hk@gmail.com>
2018-08-20 02:00:05 +0900

    style(frontend): fix a tslint error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -15,9 +15,9 @@ module.exports = withCSS(
         module: {
           rules: [
             {
-              test: /\.(graphql|gql)$/,
               exclude: /node_modules/,
               loader: 'graphql-tag/loader',
+              test: /\.(graphql|gql)$/,
             },
           ],
         },
