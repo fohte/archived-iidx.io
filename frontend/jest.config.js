bbc5111c92da0548937e5d15333cdfe5455a9403
Hayato Kawai <fohte.hk@gmail.com>
2018-08-26 15:51:09 +0900

    chore(frontend): fix typescript, babel and jest settings

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,12 +1,22 @@
-const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$'
+const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$'
 
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '{components,contexts,lib,pages,queries}/**/*.{{j,t}s{,x}}',
+  ],
+  globals: {
+    'ts-jest': {
+      tsConfigFile: '<rootDir>/tsconfig.jest.json',
+      useBabelrc: true,
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   setupFiles: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   testRegex: TEST_REGEX,
   transform: {
-    '^.+\\.tsx?$': 'babel-jest',
+    '^.+\\.jsx?$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  collectCoverage: true,
 }
