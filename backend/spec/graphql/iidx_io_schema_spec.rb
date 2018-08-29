41ee9f59c20f93ea594d691eb7c748730bb03db7
Hayato Kawai <fohte.hk@gmail.com>
2018-08-29 17:31:16 +0900

    test(backend/graphql): add tests for the musics field

--- backend/spec/graphql/iidx_io_schema_spec.rb
+++ backend/spec/graphql/iidx_io_schema_spec.rb
@@ -113,6 +113,69 @@ RSpec.describe IIDXIOSchema do
 
         include_examples 'non errors'
       end
+
+      describe 'musics field' do
+        let(:query) do
+          <<~GRAPHQL
+            query {
+              musics {
+                id
+                title
+                subTitle
+                genre
+                artist
+                textageUid
+                series
+                leggendaria
+                maps {
+                  id
+                  numNotes
+                  level
+                  playStyle
+                  difficulty
+                  minBpm
+                  maxBpm
+                }
+              }
+            }
+          GRAPHQL
+        end
+
+        let(:music) { create(:music, maps: [map]) }
+        let(:map) { build(:map, level: 12, play_style: :sp, difficulty: :another) }
+
+        before { music }
+
+        it 'returns a music with maps' do
+          expect(result['data']).to eq(
+            'musics' => [
+              {
+                'id' => music.id.to_s,
+                'title' => music.title,
+                'subTitle' => music.sub_title,
+                'genre' => music.genre,
+                'artist' => music.artist,
+                'textageUid' => music.textage_uid,
+                'series' => music.series,
+                'leggendaria' => music.leggendaria,
+                'maps' => [
+                  {
+                    'id' => map.id.to_s,
+                    'numNotes' => map.num_notes,
+                    'level' => 12,
+                    'playStyle' => 'SP',
+                    'difficulty' => 'ANOTHER',
+                    'minBpm' => map.min_bpm,
+                    'maxBpm' => map.max_bpm,
+                  },
+                ],
+              },
+            ],
+          )
+        end
+
+        include_examples 'non errors'
+      end
     end
   end
 end
