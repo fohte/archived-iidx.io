77b78784718550a7121fea16dbabb7293d605c40
Hayato Kawai <fohte.hk@gmail.com>
2018-08-29 16:55:02 +0900

    test(backend/factory): add user_profiles factory

--- backend/test/factories/users.rb
+++ backend/test/factories/users.rb
@@ -4,5 +4,11 @@ FactoryBot.define do
   factory :user do
     uid { SecureRandom.base64 }
     firebase_uid { SecureRandom.base64 }
+
+    trait :with_profile do
+      after :build do |user|
+        user.profile = build(:user_profile)
+      end
+    end
   end
 end
