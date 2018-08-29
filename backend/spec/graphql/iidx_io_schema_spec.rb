a42ef9fedda6d1b46afa39ceec840ccc9ce85186
Hayato Kawai <fohte.hk@gmail.com>
2018-08-29 16:55:25 +0900

    test(backend/graphql): add tests for viewer field

--- backend/spec/graphql/iidx_io_schema_spec.rb
+++ backend/spec/graphql/iidx_io_schema_spec.rb
@@ -14,4 +14,70 @@ RSpec.describe IIDXIOSchema do
       expect(output_directory.join('schema.graphql').read).to eq described_class.to_definition
     end
   end
+
+  describe 'schemas' do
+    shared_examples 'non errors' do
+      it 'dose not return any errors' do
+        expect(result['errors']).to be nil
+      end
+    end
+
+    subject(:result) do
+      described_class.execute(
+        query,
+        context: contexts,
+        variables: variables,
+      ).to_h
+    end
+
+    let(:contexts) { {} }
+    let(:variables) { {} }
+
+    describe 'queries' do
+      describe 'viewer field' do
+        let(:query) do
+          <<~GRAPHQL
+            query {
+              viewer {
+                id
+                uid
+                profile {
+                  id
+                  name
+                }
+              }
+            }
+          GRAPHQL
+        end
+
+        context 'when there is no current user' do
+          it 'returns nil' do
+            expect(result['data']).to eq('viewer' => nil)
+          end
+
+          include_examples 'non errors'
+        end
+
+        context 'when there is a current user' do
+          let(:viewer) { create(:user, :with_profile) }
+          let(:contexts) { { viewer: viewer } }
+
+          it 'returns the current user' do
+            expect(result['data']).to eq(
+              'viewer' => {
+                'id' => viewer.id.to_s,
+                'uid' => viewer.uid,
+                'profile' => {
+                  'id' => viewer.profile.id.to_s,
+                  'name' => viewer.profile.name,
+                },
+              },
+            )
+          end
+
+          include_examples 'non errors'
+        end
+      end
+    end
+  end
 end
