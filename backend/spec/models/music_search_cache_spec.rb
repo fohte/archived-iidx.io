194b2d175d749cb074d0a35cf4a24235f0dbfbf4
Hayato Kawai <fohte.hk@gmail.com>
2018-09-01 23:24:55 +0900

    refactor(backend): share contexts

--- backend/spec/models/music_search_cache_spec.rb
+++ backend/spec/models/music_search_cache_spec.rb
@@ -18,6 +18,30 @@ RSpec.describe MusicSearchCache do
       end
     end
 
+    shared_context 'without sub titles' do
+      let(:music) { create(:music, title: 'title', sub_title: '', series: 1) }
+
+      let(:attributes) do
+        { version: '1st&substream', title: 'title', genre: music.genre, artist: music.artist }
+      end
+    end
+
+    shared_context 'with a whitespace and a sub title' do
+      let(:music) { create(:music, title: 'title', sub_title: '(sub title)', series: 1) }
+
+      let(:attributes) do
+        { version: '1st&substream', title: 'title (sub title)', genre: music.genre, artist: music.artist }
+      end
+    end
+
+    shared_context 'with no whitespaces and a sub title' do
+      let(:music) { create(:music, title: 'title', sub_title: '(sub title)', series: 1) }
+
+      let(:attributes) do
+        { version: '1st&substream', title: 'title(sub title)', genre: music.genre, artist: music.artist }
+      end
+    end
+
     context 'when there is a record but it dose not belong to music' do
       before { music_search_cache }
 
@@ -37,32 +61,17 @@ RSpec.describe MusicSearchCache do
       end
 
       context 'without sub titles' do
-        let(:music) { create(:music, title: 'title', sub_title: '', series: 1) }
-
-        let(:attributes) do
-          { version: '1st&substream', title: 'title', genre: music.genre, artist: music.artist }
-        end
-
+        include_context 'without sub titles'
         include_examples 'search a music'
       end
 
       context 'with a whitespace and a sub title' do
-        let(:music) { create(:music, title: 'title', sub_title: '(sub title)', series: 1) }
-
-        let(:attributes) do
-          { version: '1st&substream', title: 'title (sub title)', genre: music.genre, artist: music.artist }
-        end
-
+        include_context 'with a whitespace and a sub title'
         include_examples 'search a music'
       end
 
       context 'with no whitespaces and a sub title' do
-        let(:music) { create(:music, title: 'title', sub_title: '(sub title)', series: 1) }
-
-        let(:attributes) do
-          { version: '1st&substream', title: 'title(sub title)', genre: music.genre, artist: music.artist }
-        end
-
+        include_context 'with no whitespaces and a sub title'
         include_examples 'search a music'
       end
     end
@@ -80,32 +89,17 @@ RSpec.describe MusicSearchCache do
       end
 
       context 'without sub titles' do
-        let(:music) { create(:music, title: 'title', sub_title: '', series: 1) }
-
-        let(:attributes) do
-          { version: '1st&substream', title: 'title', genre: music.genre, artist: music.artist }
-        end
-
+        include_context 'without sub titles'
         include_examples 'search a music'
       end
 
       context 'with a whitespace and a sub title' do
-        let(:music) { create(:music, title: 'title', sub_title: '(sub title)', series: 1) }
-
-        let(:attributes) do
-          { version: '1st&substream', title: 'title (sub title)', genre: music.genre, artist: music.artist }
-        end
-
+        include_context 'with a whitespace and a sub title'
         include_examples 'search a music'
       end
 
       context 'with no whitespaces and a sub title' do
-        let(:music) { create(:music, title: 'title', sub_title: '(sub title)', series: 1) }
-
-        let(:attributes) do
-          { version: '1st&substream', title: 'title(sub title)', genre: music.genre, artist: music.artist }
-        end
-
+        include_context 'with no whitespaces and a sub title'
         include_examples 'search a music'
       end
     end
