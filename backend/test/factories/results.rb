3baf68a934761db25f30c09f0c0223fd3ecb79de
Hayato Kawai <fohte.hk@gmail.com>
2018-09-03 00:38:12 +0900

    test(backend/factory): add result factory

---
+++ backend/test/factories/results.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :result do
+    score { Random.rand(0..4000) }
+    miss_count { Random.rand(0..100) }
+    clear_lamp { Result.clear_lamp.values.sample }
+    grade { Result.grade.values.sample }
+    last_played_at { Faker::Date.backward }
+  end
+end
