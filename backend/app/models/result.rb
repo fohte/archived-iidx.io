7c751257e8a5c0aa3fe0a04d85e80b2adbd69280
Hayato Kawai <fohte.hk@gmail.com>
2018-09-02 23:12:30 +0900

    feat(backend/model): add the failed clear lamp

--- backend/app/models/result.rb
+++ backend/app/models/result.rb
@@ -4,6 +4,6 @@ class Result < ApplicationRecord
   belongs_to :user
   belongs_to :map
 
-  enumerize :clear_lamp, in: %i[assist easy normal hard ex_hard full_combo]
+  enumerize :clear_lamp, in: %i[failed assist easy normal hard ex_hard full_combo]
   enumerize :grade, in: %i[f e d c b a aa aaa]
 end
