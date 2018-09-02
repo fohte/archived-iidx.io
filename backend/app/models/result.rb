b2de880a9cb998b43ac6b9740a68fca0f4209632
Hayato Kawai <fohte.hk@gmail.com>
2018-09-02 23:11:23 +0900

    feat(backend/model): remove *_plus grade enums

--- backend/app/models/result.rb
+++ backend/app/models/result.rb
@@ -5,5 +5,5 @@ class Result < ApplicationRecord
   belongs_to :map
 
   enumerize :clear_lamp, in: %i[assist easy normal hard ex_hard full_combo]
-  enumerize :grade, in: %i[f e d c b a a_plus aa aa_plus aaa aaa_plus]
+  enumerize :grade, in: %i[f e d c b a aa aaa]
 end
