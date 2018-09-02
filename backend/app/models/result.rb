fcacf8cecb1fed8816f42a675ba598a2b3e6c0fe
Hayato Kawai <fohte.hk@gmail.com>
2018-09-02 23:47:37 +0900

    fix(backend/model): use integer values on enums

--- backend/app/models/result.rb
+++ backend/app/models/result.rb
@@ -4,6 +4,24 @@ class Result < ApplicationRecord
   belongs_to :user
   belongs_to :map
 
-  enumerize :clear_lamp, in: %i[failed assist easy normal hard ex_hard full_combo]
-  enumerize :grade, in: %i[f e d c b a aa aaa]
+  enumerize :clear_lamp, in: {
+    failed: 1,
+    assist: 2,
+    easy: 3,
+    normal: 4,
+    hard: 5,
+    ex_hard: 6,
+    full_combo: 7,
+  }
+
+  enumerize :grade, in: {
+    f: 1,
+    e: 2,
+    d: 3,
+    c: 4,
+    b: 5,
+    a: 6,
+    aa: 7,
+    aaa: 8,
+  }
 end
