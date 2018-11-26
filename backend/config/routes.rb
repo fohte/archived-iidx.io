3467562dcfda3003cda082ce4204330d5b760b35
Hayato Kawai <fohte.hk@gmail.com>
2018-11-26 13:32:35 +0900

    feat(backend): add root path as a healthcheck endpoint

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/health', to: ->(_env) { [200, { 'Content-Type' => 'application/json' }, []] }
+  empty_response = ->(_env) { [200, { 'Content-Type' => 'application/json' }, []] }
+
+  root to: empty_response
+  get '/health', to: empty_response
 
   post '/graphql', to: 'graphql#execute'
 end
