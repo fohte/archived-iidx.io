aa0637114ef752cd23754bdcd02b4b706d8aec9c
Hayato Kawai <fohte.hk@gmail.com>
2018-12-03 02:51:35 +0900

    test(frontend): add snapshot tests for the profile page

---
+++ frontend/__tests__/pages/profile.test.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import * as renderer from 'react-test-renderer'
+
+import Profile from '@app/pages/profile'
+
+describe('/', () => {
+  it('renders correctly', () => {
+    const component = renderer.create(
+      <Profile user={{ name: 'username' }} loading={false} />,
+    )
+    const tree = component.toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly when loading', () => {
+    const component = renderer.create(<Profile loading />)
+    const tree = component.toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly if there are errors', () => {
+    const component = renderer.create(
+      <Profile
+        errors={[{ code: 'NOT_FOUND', message: 'not found' }]}
+        loading={false}
+      />,
+    )
+    const tree = component.toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
