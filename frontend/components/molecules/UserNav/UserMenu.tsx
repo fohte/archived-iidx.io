b8236e69e5058c28ebfc1454992b436354c3bf7c
Hayato Kawai <fohte.hk@gmail.com>
2018-12-03 01:39:25 +0900

    feat(frontend): add a link to the user profile page in the user menu

--- frontend/components/molecules/UserNav/UserMenu.tsx
+++ frontend/components/molecules/UserNav/UserMenu.tsx
@@ -11,6 +11,10 @@ export interface Props {
 const UserMenu: React.SFC<Props> = ({ displayName }) => (
   <Dropdown item text={displayName} pointing>
     <Dropdown.Menu>
+      <Link route={`/@${displayName}`}>
+        <Dropdown.Item>Profile</Dropdown.Item>
+      </Link>
+      <Dropdown.Divider />
       <Link route="/results/new">
         <Dropdown.Item>Register results</Dropdown.Item>
       </Link>
