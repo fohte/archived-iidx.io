b00ed24d046cbbc74f7f4182d9a0498582c09e5e
Hayato Kawai <fohte.hk@gmail.com>
2019-01-03 17:07:09 +0900

    feat(frontend): add a link to musics page temporary

--- frontend/components/organisms/Profile.tsx
+++ frontend/components/organisms/Profile.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Header } from 'semantic-ui-react'
 
 import { FindUserUser } from '@app/queries'
+import { Link } from '@app/routes'
 
 export type Props = {
   user: FindUserUser
@@ -10,6 +11,9 @@ export type Props = {
 const Profile: React.SFC<Props> = ({ user }) => (
   <>
     <Header as="h2">@{user.name}</Header>
+    <Link route="musics" params={{ screenName: user.name }}>
+      <a>musics</a>
+    </Link>
   </>
 )
 export default Profile
