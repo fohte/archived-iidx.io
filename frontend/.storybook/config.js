ec1f5a25f6737b0042a9f87f2586ecaca557d4bb
Hayato Kawai <fohte.hk@gmail.com>
2019-01-09 23:49:34 +0900

    feat(storybook): add and setup addons

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,4 +1,40 @@
-import { configure } from '@storybook/react'
+import { addDecorator, configure } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+
+import { setOptions } from '@storybook/addon-options'
+
+setOptions({
+  hierarchyRootSeparator: /\|/,
+})
+
+addDecorator(
+  withInfo({
+    styles: {
+      header: {
+        h1: {
+          marginRight: '20px',
+          fontSize: '25px',
+          display: 'inline',
+        },
+        body: {
+          paddingTop: 0,
+          paddingBottom: 0,
+        },
+        h2: {
+          display: 'inline',
+          color: '#999',
+        },
+      },
+      infoBody: {
+        backgroundColor: '#eee',
+        padding: '0px 5px',
+        lineHeight: '2',
+      },
+    },
+    inline: true,
+    source: false,
+  }),
+)
 
 const req = require.context('../stories', true, /.stories.tsx$/)
 
