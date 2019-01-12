646f2396f3487c95c06a9f317e464e514786e3a3
Hayato Kawai <fohte.hk@gmail.com>
2019-01-12 20:41:03 +0900

    feat(frontend/storybook): add knobs addon

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,11 +1,15 @@
 import 'semantic-ui-css/semantic.min.css'
 import './style.css'
-import { configure } from '@storybook/react'
+
+import { withKnobs } from '@storybook/addon-knobs'
+import { configure, addDecorator } from '@storybook/react'
 import { setOptions } from '@storybook/addon-options'
 
 setOptions({
   hierarchyRootSeparator: /\|/,
 })
+
+addDecorator(withKnobs)
 
 const req = require.context('../stories', true, /.stories.tsx$/)
 
