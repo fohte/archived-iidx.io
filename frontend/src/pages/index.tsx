cb558ca628cbac83d7a238bde3d8878b2af6314b
Hayato Kawai <fohte.hk@gmail.com>
2019-02-24 20:41:43 +0900

    feat(frontend): remove MusicTable component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,26 +1,4 @@
-import MusicTable from '@app/components/organisms/MusicTable'
 import MainLayout from '@app/components/templates/MainLayout'
-import {
-  GetMusicsWithMapsComponent,
-  GetMusicsWithMapsDocument,
-} from '@app/queries'
 import * as React from 'react'
 
-export default () => (
-  <MainLayout>
-    <GetMusicsWithMapsComponent query={GetMusicsWithMapsDocument}>
-      {({ loading, error, data }) => {
-        if (loading) {
-          return 'loading'
-        }
-        if (error || !data) {
-          return 'error'
-        }
-
-        if (data.musics) {
-          return <MusicTable musics={data.musics} />
-        }
-      }}
-    </GetMusicsWithMapsComponent>
-  </MainLayout>
-)
+export default () => <MainLayout />
