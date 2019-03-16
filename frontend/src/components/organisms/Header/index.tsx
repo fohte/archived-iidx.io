cb152edf7fd073fd091380f115070c00053998f1
Hayato Kawai <fohte.hk@gmail.com>
2019-03-16 22:52:04 +0900

    feat(frontend): temporarily implement the header css

--- frontend/src/components/organisms/Header/index.tsx
+++ frontend/src/components/organisms/Header/index.tsx
@@ -1,26 +1,30 @@
 import * as React from 'react'
-import { Container, Menu, Segment } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
 
 import Brand from '@app/components/atoms/Brand'
+import Container from '@app/components/atoms/Container'
 import UserNav from '@app/components/molecules/UserNav'
 import withAuthState from '@app/lib/withAuthState'
 import { Link } from '@app/routes'
+import * as css from './style.scss'
 
 const Header: React.SFC = () => (
-  <Segment vertical inverted>
-    <Menu secondary inverted>
-      <Container>
-        <Link route="/" prefetch passHref>
-          <Menu.Item as="a">
-            <Brand />
-          </Menu.Item>
-        </Link>
-        <Menu.Menu position="right">
+  <nav className={css.headerNav}>
+    <Container>
+      <div className={css.flexContainer}>
+        <div className={css.item}>
+          <Link route="/" prefetch passHref>
+            <Menu.Item as="a">
+              <Brand />
+            </Menu.Item>
+          </Link>
+        </div>
+        <div className={css.item}>
           <EnhancedUserNav />
-        </Menu.Menu>
-      </Container>
-    </Menu>
-  </Segment>
+        </div>
+      </div>
+    </Container>
+  </nav>
 )
 
 const EnhancedUserNav = withAuthState()(UserNav)
