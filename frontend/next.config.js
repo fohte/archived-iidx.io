81bd5e91bbd6237b33a972bc74f25d49a4fbf8d5
Hayato Kawai <fohte.hk@gmail.com>
2019-03-16 18:43:06 +0900

    build(frontend): introduce sass with css modules

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge')
 const path = require('path')
 const withCSS = require('@zeit/next-css')
+const withSass = require('@zeit/next-sass')
 const withTypescript = require('@zeit/next-typescript')
 
 if (typeof require !== 'undefined') {
@@ -8,38 +9,41 @@ if (typeof require !== 'undefined') {
 }
 
 module.exports = withCSS(
-  withTypescript({
-    webpack(config, options) {
-      return merge(config, {
-        resolve: { alias: { '@app': path.join(__dirname, 'src') } },
-        module: {
-          rules: [
-            {
-              test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
-              use: {
-                loader: 'url-loader',
-                options: {
-                  limit: 100000,
-                  publicPath: './',
-                  outputPath: 'static/',
-                  name: '[name].[ext]',
+  withSass({
+    cssModules: true,
+    ...withTypescript({
+      webpack(config, options) {
+        return merge(config, {
+          resolve: { alias: { '@app': path.join(__dirname, 'src') } },
+          module: {
+            rules: [
+              {
+                test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+                use: {
+                  loader: 'url-loader',
+                  options: {
+                    limit: 100000,
+                    publicPath: './',
+                    outputPath: 'static/',
+                    name: '[name].[ext]',
+                  },
                 },
               },
-            },
-          ],
-        },
-      })
-    },
-    generateBuildId: () => process.env.IIDXIO_VERSION,
-    serverRuntimeConfig: {
-      privateApiUrl: process.env.PRIVATE_API_URL,
-    },
-    publicRuntimeConfig: {
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      publicApiUrl: process.env.PUBLIC_API_URL,
-      version: process.env.IIDXIO_VERSION,
-    },
+            ],
+          },
+        })
+      },
+      generateBuildId: () => process.env.IIDXIO_VERSION,
+      serverRuntimeConfig: {
+        privateApiUrl: process.env.PRIVATE_API_URL,
+      },
+      publicRuntimeConfig: {
+        firebaseApiKey: process.env.FIREBASE_API_KEY,
+        firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+        publicApiUrl: process.env.PUBLIC_API_URL,
+        version: process.env.IIDXIO_VERSION,
+      },
+    }),
   }),
 )
