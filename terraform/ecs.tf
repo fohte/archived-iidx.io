17aa53660f69e266790dfc6e22399aae7f49b4fa
Hayato Kawai <fohte.hk@gmail.com>
2019-05-26 11:20:58 +0900

    refactor(tf): container definition を HCL で定義する

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,26 @@
+locals {
+  logConfigurations = {
+    web = {
+      logDriver = "awslogs"
+
+      options = {
+        awslogs-group         = "${aws_cloudwatch_log_group.app.name}"
+        awslogs-region        = "ap-northeast-1"
+        awslogs-stream-prefix = "web"
+      }
+    }
+    sidecar = {
+      logDriver = "awslogs"
+
+      options = {
+        awslogs-group         = "${aws_cloudwatch_log_group.app.name}"
+        awslogs-region        = "ap-northeast-1"
+        awslogs-stream-prefix = "sidecars"
+      }
+    }
+  }
+}
+
 resource "aws_ecs_cluster" "main" {
   name = replace(local.name, "/[^a-zA-Z0-9\\-]/", "-")
 }
@@ -12,40 +35,130 @@ resource "aws_ecs_service" "web" {
   deployment_minimum_healthy_percent = 0
 }
 
-
 resource "aws_ecs_task_definition" "web" {
   family = "${replace(local.name, "/[^a-zA-Z0-9\\-]/", "-")}-web"
-  container_definitions = templatefile("templates/container_definitions/web.json", {
-    db_host          = aws_db_instance.main.address
-    secret_key_base  = data.aws_ssm_parameter.secret_key_base.value
-    db_password      = data.aws_ssm_parameter.db_password.value
-    log_group_name   = aws_cloudwatch_log_group.app.name
-    log_group_region = "ap-northeast-1"
-  })
+
+  container_definitions = jsonencode([
+    {
+      name  = "backend"
+      image = "iidxio/backend"
+
+      environment = [for k, v in {
+        "APP_DATABASE_PASSWORD" = "${data.aws_ssm_parameter.db_password.value}"
+
+        "FIREBASE_API_KEY"     = "AIzaSyCO-OMhKvm89aZj4-1HTZOc0PTnb5aUtxQ"
+        "FIREBASE_AUTH_DOMAIN" = "iidx-io.firebaseapp.com"
+        "FIREBASE_PROJECT_ID"  = "iidx-io"
+
+        "MYSQL_HOST" = "${aws_db_instance.main.address}"
+        "MYSQL_PORT" = "3306"
+
+        "RAILS_LOG_TO_STDOUT" = "true"
+
+        "REDIS_HOST" = "redis"
+        "REDIS_PORT" = "6379"
+
+        "SECRET_KEY_BASE" = "${data.aws_ssm_parameter.secret_key_base.value}"
+      } : { name = k, value = v }]
+
+      links = ["redis"]
+
+      memoryReservation = 256
+
+      portMappings = [{
+        containerPort = 3000
+        hostPort      = 5000
+        protocol      = "tcp"
+      }]
+
+      logConfiguration = local.logConfigurations.web
+    },
+    {
+      name  = "frontend"
+      image = "iidxio/frontend"
+
+      environment = [for k, v in {
+        "PRIVATE_API_URL" = "http://backend:3000/graphql"
+        "PUBLIC_API_URL"  = "https://api.iidx.io/graphql"
+
+        "FIREBASE_API_KEY"     = "AIzaSyCO-OMhKvm89aZj4-1HTZOc0PTnb5aUtxQ"
+        "FIREBASE_AUTH_DOMAIN" = "iidx-io.firebaseapp.com"
+        "FIREBASE_PROJECT_ID"  = "iidx-io"
+      } : { name = k, value = v }]
+
+      links = ["backend"]
+
+      memoryReservation = 256
+
+      logConfiguration = local.logConfigurations.web
+
+      portMappings = [{
+        containerPort = 3000
+        hostPort      = 3000
+        protocol      = "tcp"
+      }]
+    },
+    {
+      name  = "redis"
+      image = "redis:4.0.11-stretch"
+
+      memoryReservation = 128
+
+      logConfiguration = local.logConfigurations.web
+    }
+  ])
 }
 
 resource "aws_ecs_task_definition" "ridgepole" {
   family = "${replace(local.name, "/[^a-zA-Z0-9\\-]/", "-")}-ridgepole"
 
-  container_definitions = templatefile("templates/container_definitions/ridgepole.json", {
-    db_host          = aws_db_instance.main.address
-    secret_key_base  = data.aws_ssm_parameter.secret_key_base.value
-    db_password      = data.aws_ssm_parameter.db_password.value
-    log_group_name   = aws_cloudwatch_log_group.app.name
-    log_group_region = "ap-northeast-1"
-  })
-}
+  container_definitions = jsonencode([
+    {
+      name  = "ridgepole"
+      image = "iidxio/backend"
 
+      environment = [for k, v in {
+        "APP_DATABASE_PASSWORD" = "${data.aws_ssm_parameter.db_password.value}"
+
+        "MYSQL_HOST" = "${aws_db_instance.main.address}"
+        "MYSQL_PORT" = "3306"
+
+        "RAILS_LOG_TO_STDOUT" = "true"
+      } : { name = k, value = v }]
+
+      command = ["bundle", "exec", "rails", "db:schema:apply", "RAILS_ENV=production"]
+
+      memoryReservation = 64
+
+      logConfiguration = local.logConfigurations.sidecar
+    }
+  ])
+}
 
 resource "aws_ecs_task_definition" "textage_scraper" {
   family = "${replace(local.name, "/[^a-zA-Z0-9\\-]/", "-")}-textage-scraper"
-  container_definitions = templatefile("templates/container_definitions/textage_scraper.json", {
-    db_host          = aws_db_instance.main.address
-    secret_key_base  = data.aws_ssm_parameter.secret_key_base.value
-    db_password      = data.aws_ssm_parameter.db_password.value
-    log_group_name   = aws_cloudwatch_log_group.app.name
-    log_group_region = "ap-northeast-1"
-  })
+
+  container_definitions = jsonencode([
+    {
+      name  = "textage_scraper"
+      image = "iidxio/backend"
+
+      environment = [for k, v in {
+        "APP_DATABASE_PASSWORD" = "${data.aws_ssm_parameter.db_password.value}"
+
+        "MYSQL_HOST" = "${aws_db_instance.main.address}"
+        "MYSQL_PORT" = "3306"
+
+        "RAILS_LOG_TO_STDOUT" = "true"
+      } : { name = k, value = v }]
+
+      command = ["bundle", "exec", "rails", "textage:crawl"]
+
+      memoryReservation = 64
+
+      logConfiguration = local.logConfigurations.sidecar
+    }
+  ])
 }
 
 resource "aws_cloudwatch_event_rule" "textage_scraper" {
