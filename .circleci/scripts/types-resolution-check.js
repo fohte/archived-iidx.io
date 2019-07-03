a674ce67582b8d8847eb4807fe4754efc6d7f79a
Hayato Kawai <fohte.hk@gmail.com>
2019-07-03 20:15:56 +0900

    ci(): @types パッケージに resolutions が定義されているか確認する
    dependencies, devDependencies に @types が書かれているとき、
    resolutions にも書くようにしているが、書き忘れていたときに
    気づきにくい。その対策として、CI でチェックするようにする。

---
+++ .circleci/scripts/types-resolution-check.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+
+const repoRoot = path.join(__dirname, '../..')
+
+const packages = JSON.parse(
+  fs.readFileSync(path.join(repoRoot, 'frontend/package.json')),
+)
+
+const deps = { ...packages.dependencies, ...packages.devDependencies }
+const typePackages = Object.keys(deps).reduce((acc, key) => {
+  if (/^@types\//.test(key)) {
+    acc[key] = deps[key]
+  }
+
+  return acc
+}, {})
+
+const _diff = (xs, ys) => xs.filter(x => !new Set(ys).has(x))
+
+const diff = (xs, ys) => _diff(xs, ys).concat(_diff(ys, xs))
+
+const packageDiffs = diff(
+  Object.keys(typePackages),
+  Object.keys(packages.resolutions),
+)
+
+if (packageDiffs.length !== 0) {
+  console.error('missing packages:', ...packageDiffs)
+  process.exit(1)
+}
