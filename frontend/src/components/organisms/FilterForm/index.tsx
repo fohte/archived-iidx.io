59d240e51a73688ccd843305d732667eb76eba85
Hayato Kawai <fohte.hk@gmail.com>
2019-07-06 21:18:59 +0900

    fix(frontend): FilterForm 表示時に背景のスクロール位置を固定する

--- frontend/src/components/organisms/FilterForm/index.tsx
+++ frontend/src/components/organisms/FilterForm/index.tsx
@@ -10,6 +10,31 @@ export type Props = {
 }
 
 const FilterForm: React.SFC<Props> = ({ onCloseRequested }) => {
+  // このコンポーネントが開いている間は背景のスクロールを無効化する
+  React.useEffect(() => {
+    const { top, position } = document.body.style
+
+    // `documentElement` は古いブラウザで未サポートなので `body` に fallback する
+    // @see https://ginpen.com/2017/10/06/chrome-61-scrolltop/
+    const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop
+
+    // position を変更すると背景が最上部までスクロールされるので
+    // 擬似的に背景位置を固定する
+    document.body.style.top = `-${scrollTop}px`
+
+    document.body.style.position = 'fixed'
+
+    return () => {
+      document.body.style.top = top
+      document.body.style.position = position
+
+      // position を変更すると背景が最上部までスクロールされるので
+      // スクロール位置をコンポーネント表示前のスクロール位置に戻す
+      document.documentElement.scrollTop = document.body.scrollTop = scrollTop
+    }
+  })
+
   return (
     <div className={cx('filter-form')}>
       <div
