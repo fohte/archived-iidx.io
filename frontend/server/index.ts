e77f18c48a3b3f1304479b669088244cb741e41a
Hayato Kawai <fohte.hk@gmail.com>
2019-07-20 15:13:22 +0900

    feat(frontend): server スクリプトも TypeScript で書くようにする
    Next.js が --isolatedModules を強制してくるので require
    を使えなくなったため

--- frontend/server/index.ts
+++ frontend/server/index.ts
@@ -1,13 +1,13 @@
-const path = require('path')
-const next = require('next')
-const routes = require('./src/routes')
+import express from 'express'
+import next from 'next'
+import path from 'path'
+
+import routes from '@app/routes'
 
 const app = next({
   dev: process.env.NODE_ENV !== 'production',
 })
 const handler = routes.getRequestHandler(app)
-
-const express = require('express')
 
 app.prepare().then(() => {
   const server = express()
--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,6 +1,6 @@
-const routes = require('@yolkai/next-routes').default
+import routes from '@yolkai/next-routes'
 
-module.exports = routes()
+export default routes()
   .add('user', '/@:screenName/:playStyle?', 'profile')
   .add('map', '/@:screenName/:playStyle/musics/:musicId/:difficulty', 'map')
   .add('musics', '/@:screenName/:playStyle/musics', 'musics')
