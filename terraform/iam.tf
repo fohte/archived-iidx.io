b30339293d53a58158e48b2bc30d7c7180a67dcf
Hayato Kawai <fohte.hk@gmail.com>
2019-08-04 16:26:56 +0900

    feat(tf): circleci ユーザーに ecs-deploy に必要な権限を与える
    @see https://github.com/silinternational/ecs-deploy#aws-iam-policy-configuration
    
    Terraform plan:
    
    ```
    An execution plan has been generated and is shown below.
    Resource actions are indicated with the following symbols:
      + create
    
    Terraform will perform the following actions:
    
      # aws_iam_policy.ecs_deploy will be created
      + resource "aws_iam_policy" "ecs_deploy" {
          + arn    = (known after apply)
          + id     = (known after apply)
          + name   = "ECSDeploy"
          + path   = "/"
          + policy = jsonencode(
                {
                  + Statement = [
                      + {
                          + Action   = [
                              + "iam:PassRole",
                              + "ecs:UpdateService",
                              + "ecs:StopTask",
                              + "ecs:StartTask",
                              + "ecs:RegisterTaskDefinition",
                              + "ecs:ListTasks",
                              + "ecs:ListTaskDefinitions",
                              + "ecs:DescribeTasks",
                              + "ecs:DescribeTaskDefinition",
                              + "ecs:DescribeServices",
                              + "ecs:DeregisterTaskDefinition",
                            ]
                          + Effect   = "Allow"
                          + Resource = "*"
                          + Sid      = ""
                        },
                    ]
                  + Version   = "2012-10-17"
                }
            )
        }
    
      # aws_iam_user_policy_attachment.circleci_ecs_deploy will be created
      + resource "aws_iam_user_policy_attachment" "circleci_ecs_deploy" {
          + id         = (known after apply)
          + policy_arn = (known after apply)
          + user       = "circleci"
        }
    
    Plan: 2 to add, 0 to change, 0 to destroy.
    ```

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -7,3 +7,33 @@ resource "aws_iam_user_policy_attachment" "circleci_ecr" {
   user       = aws_iam_user.circleci.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
+
+data "aws_iam_policy_document" "ecs_deploy" {
+  statement {
+    actions = [
+      "ecs:DeregisterTaskDefinition",
+      "ecs:DescribeServices",
+      "ecs:DescribeTaskDefinition",
+      "ecs:DescribeTasks",
+      "ecs:ListTasks",
+      "ecs:ListTaskDefinitions",
+      "ecs:RegisterTaskDefinition",
+      "ecs:StartTask",
+      "ecs:StopTask",
+      "ecs:UpdateService",
+      "iam:PassRole",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ecs_deploy" {
+  name   = "ECSDeploy"
+  policy = data.aws_iam_policy_document.ecs_deploy.json
+}
+
+resource "aws_iam_user_policy_attachment" "circleci_ecs_deploy" {
+  user       = aws_iam_user.circleci.name
+  policy_arn = aws_iam_policy.ecs_deploy.arn
+}
