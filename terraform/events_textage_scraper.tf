0a27ec3040ac60fb185a38152d0f6a8085ae5c11
Hayato Kawai <fohte.hk@gmail.com>
2019-09-17 22:05:30 +0900

    refactor(tf): CloudWatch Events 系のファイルを分割する

---
+++ terraform/events_textage_scraper.tf
@@ -0,0 +1,28 @@
+resource "aws_cloudwatch_event_rule" "textage_scraper" {
+  name        = "${local.name}.textage_scraper"
+  description = "Run the textage scraper task nightly"
+
+  # 毎日 JST 2:00 に実行する
+  schedule_expression = "cron(0 17 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "textage_scraper" {
+  target_id = "${local.name}.textage_scraper"
+  arn       = aws_ecs_cluster.main.arn
+  rule      = aws_cloudwatch_event_rule.textage_scraper.name
+  role_arn  = aws_iam_role.rake_events.arn
+
+  ecs_target {
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.rake.arn
+  }
+
+  input = jsonencode({
+    "containerOverrides" = [
+      {
+        name    = "textage_scraper"
+        command = ["bundle", "exec", "rails", "textage:crawl"]
+      }
+    ]
+  })
+}
