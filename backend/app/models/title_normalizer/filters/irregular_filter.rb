ff16ca3fdb489420c6303889141acd745c88a9dd
Hayato Kawai <fohte.hk@gmail.com>
2019-09-28 22:27:04 +0900

    fix(backend/title_normalizer): 対応しきれなかったものを irregular filter に突っ込む

--- backend/app/models/title_normalizer/filters/irregular_filter.rb
+++ backend/app/models/title_normalizer/filters/irregular_filter.rb
@@ -8,9 +8,12 @@ module TitleNormalizer
         'Blind Justice 〜Torn souls,Hurt Faiths〜' => 'Blind Justice ～Torn souls， Hurt Faiths ～',
         'CROSSROAD' => 'CROSSROAD ～Left Story～',
         'City Never Sleeps (IIDX EDITION)' => 'City Never Sleeps (IIDX Edition)',
+        'Colors -Y&Co. Eurobeat Remix-' => 'Colors -Y&Co.Eurobeat Remix-',
+        'Usual Days-remix' => 'Usual Days -remix',
         'キャトられ♥恋はモ〜モク' => 'キャトられ恋はモ～モク',
         'パラドキシカル・タイムリープトライアル (Short.Ver)' => 'パラドキシカル・タイムリープトライアル (Short Ver.)',
         '共犯ヘヴンズコード' => '共犯へヴンズコード',
+        '超!!遠距離らぶ♥メ〜ル' => '超!!遠距離らぶ?メ～ル',
       }.freeze
 
       def call(str)
