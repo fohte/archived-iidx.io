b609368b3756d1c2329229c200631cc13436d082
Hayato Kawai <fohte.hk@gmail.com>
2019-09-30 19:10:12 +0900

    chore(backend, frontend): production でだけ Sentry を有効化する

--- backend/config/initializers/raven.rb
+++ backend/config/initializers/raven.rb
@@ -2,4 +2,5 @@
 
 Raven.configure do |config|
   config.dsn = 'https://3bc778da22004203b06e68b90fb44967:dbcd529ce8904bad9d1517c68920dc13@sentry.io/1764918'
+  config.environments = %w[production]
 end
