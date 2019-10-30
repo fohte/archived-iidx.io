f9681a2011e848ea680f1b5d8f9298354f16b832
Hayato Kawai <fohte.hk@gmail.com>
2019-10-31 08:40:09 +0900

    feat(backend): results, result_logs の series カラムを not null にする
    oneshot タスクで series カラムに値を挿入済みのため

--- backend/db/schemas/result_logs.rb
+++ backend/db/schemas/result_logs.rb
@@ -9,8 +9,7 @@ create_table :result_logs, force: :cascade, id: :bigint, unsigned: true, options
   t.integer :clear_lamp, null: true # enum
   t.string :grade, null: true # enum
 
-  # FIXME: 全レコードに値を設定し終わったら null: false にする
-  t.integer :series, null: true # enum
+  t.integer :series, null: false # enum
 
   t.datetime :last_played_at, null: false
   t.datetime :created_at, null: false
